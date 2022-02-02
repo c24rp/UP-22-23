e62489ce56a5e1cb8a8b26460a95944f7d6c3ffb
c23sd <c23sd@dalton.org>
2022-02-02 17:38:55 -0500

    test servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
@@ -7,144 +7,148 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-@TeleOp(name = "TankTeleOp", group = "TeleOp")
-public class TankTeleOp extends OpMode {
 
-    private ElapsedTime runtime = new ElapsedTime();
+    @TeleOp(name = "TankTeleOp", group = "TeleOp")
+    public class TankTeleOp extends OpMode {
 
-    private MecanumRobot rb = new MecanumRobot();
+        private ElapsedTime runtime = new ElapsedTime();
 
-    double liftmotorStartingPosition;
-    double servoBoxStartingPosition = 0;
+        private MecanumRobot rb = new MecanumRobot();
 
-    double frontLeftPower;
-    double backLeftPower;
-    double frontRightPower;
-    double backRightPower;
+        double liftmotorStartingPosition;
+        double servoBoxStartingPosition;
 
-    @Override
-    public void init() {
-        telemetry.addData("Status", "Initializing");
+        double frontLeftPower;
+        double backLeftPower;
+        double frontRightPower;
+        double backRightPower;
 
-        rb.init(hardwareMap, null);
+        @Override
+        public void init() {
+            telemetry.addData("Status", "Initializing");
+
+            rb.init(hardwareMap, null);
+
+            double liftmotorStartingPosition = rb.liftmotor.getCurrentPosition();
+            double servoBoxStartingPosition = rb.boxServo.getPosition();
 
 
-        telemetry.addData("Status", "Initialized");
+            telemetry.addData("Status", "Initialized");
+        }
+
+        @Override
+        public void start() {
+            runtime.reset();
+            rb.resetEncoder(rb.liftmotor);
+
+        }
+
+        @Override
+        public void loop() {
+            telemetry.addData("Status", "Looping");
+
+            driveChassis();
+
+            lift();
+
+            intake();
+
+            liftEncoder();
+
+            servoBox();
+
+            telemetry.update();
+        }
+
+
+
+        //moves the lift up
+        private void lift() {
+            if(gamepad1.right_bumper){
+                rb.liftmotor.setPower(1);
+            }
+            else if(gamepad1.left_bumper){
+                rb.liftmotor.setPower(-1);
+            }
+            else {
+                rb.liftmotor.setPower(0);
+            }
+        }
+
+        private void liftEncoder() {
+            double x = rb.liftmotor.getCurrentPosition();
+            if(gamepad1.dpad_right && x < liftmotorStartingPosition + 400){
+                rb.liftmotor.setPower(1);
+            }
+            if(gamepad1.dpad_left && x > liftmotorStartingPosition){
+                rb.liftmotor.setPower(-1);
+            }
+            else {
+                rb.liftmotor.setPower(0);
+            }
+        }
+
+        private void servoBox() {
+            if(gamepad2.dpad_down) {
+                rb.boxServo.setPosition(servoBoxStartingPosition - 0.6);
+            }
+            else if(gamepad2.dpad_right) {
+                rb.boxServo.setPosition(servoBoxStartingPosition - 0.25);
+            }
+            else {
+                rb.boxServo.setPosition(servoBoxStartingPosition);
+            }
+
+        }
+
+
+        private void intake() {
+            if(gamepad1.x){
+                rb.intakemotor.setPower(1);
+            }
+            else if(gamepad1.b){
+                rb.intakemotor.setPower(-1);
+            }
+            else {
+                rb.intakemotor.setPower(0);
+            }
+        }
+
+
+
+
+
+        private void driveChassis() {
+            double y = -gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_y * 1;
+
+
+
+            if(Math.abs(y) > Math.abs(rx)){
+                frontLeftPower = rx;
+                backLeftPower = rx;
+                frontRightPower = -rx;
+                backRightPower = -rx;
+            }
+            else if(Math.abs(y) < Math.abs(rx)){
+                frontLeftPower = y;
+                backLeftPower = y;
+                frontRightPower = y;
+                backRightPower = y;
+            }
+
+            else {
+                frontLeftPower = 0;
+                backLeftPower = 0;
+                frontRightPower = 0;
+                backRightPower = 0;
+            }
+
+            rb.flMotor.setPower(frontLeftPower);
+            rb.blMotor.setPower(backLeftPower);
+            rb.frMotor.setPower(frontRightPower);
+            rb.brMotor.setPower(backRightPower);
+        }
     }
 
-    @Override
-    public void start() {
-        runtime.reset();
-        rb.resetEncoder(rb.liftmotor);
-    }
-
-    @Override
-    public void loop() {
-        telemetry.addData("Status", "Looping");
-
-        driveChassis();
-
-        lift();
-
-        intake();
-
-        telemetry.update();
-    }
-
-
-
-    //moves the lift up
-    private void lift() {
-        if(gamepad1.right_bumper){
-            rb.liftmotor.setPower(1);
-        }
-        if(gamepad1.left_bumper){
-            rb.liftmotor.setPower(-1);
-        }
-        else {
-            rb.liftmotor.setPower(0);
-        }
-    }
-
-
-    private void intake() {
-        if(gamepad1.x){
-            rb.intakemotor.setPower(1);
-        }
-        if(gamepad1.b){
-            rb.intakemotor.setPower(-1);
-        }
-        else {
-            rb.intakemotor.setPower(0);
-        }
-    }
-
-
-
-
-
-    private void driveChassis() {
-        double y = -gamepad1.left_stick_y;
-        double rx = gamepad1.right_stick_x * 1;
-
-
-
-        if(Math.abs(y) > Math.abs(rx)){
-            frontLeftPower = y;
-            backLeftPower = y;
-            frontRightPower = y;
-            backRightPower = y;
-        }
-        else if(Math.abs(y) < Math.abs(rx)){
-            frontLeftPower = rx;
-            backLeftPower = rx;
-            frontRightPower = -rx;
-            backRightPower = -rx;
-        }
-
-        else {
-            frontLeftPower = 0;
-            backLeftPower = 0;
-            frontRightPower = 0;
-            backRightPower = 0;
-        }
-
-        rb.flMotor.setPower(frontLeftPower);
-        rb.blMotor.setPower(backLeftPower);
-        rb.frMotor.setPower(frontRightPower);
-        rb.brMotor.setPower(backRightPower);
-
-
-//        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-//        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-//        double rightX = gamepad1.right_stick_x;
-//        final double v1 = r * Math.cos(robotAngle) + rightX;
-//        final double v2 = r * Math.sin(robotAngle) - rightX;
-//        final double v3 = r * Math.sin(robotAngle) + rightX;
-//        final double v4 = r * Math.cos(robotAngle) - rightX;
-//
-//        rb.flMotor.setPower(v1);
-//        rb.blMotor.setPower(v2);
-//        rb.frMotor.setPower(v3);
-//        rb.brMotor.setPower(v4);
-
-
-//        float leftY = -gamepad1.left_stick_y;
-//        float leftX = gamepad1.left_stick_x;
-//        float rightX = gamepad1.right_stick_x;
-//
-//        double pow;
-//        if (gamepad1.right_trigger >= TRIGGER_THRESHOLD) {
-//            pow = DRIVE_POWER_SLOW;
-//        } else {
-//            pow = DRIVE_POWER;
-//        }
-//
-//        if (leftX * leftX + leftY * leftY >= DRIVE_STICK_THRESHOLD_SQUARED || Math.abs(rightX) >= DRIVE_STICK_THRESHOLD) {
-//            rb.drive(leftX, leftY, rightX, pow);
-//        } else {
-//            rb.driveStop();
-//        }
-    }
-}
 
