fa1fdf7ae805d00d28f743cb9f2db91d2d75e7a5
jai-kapoor <72400158+jai-kapoor@users.noreply.github.com>
2022-02-18 18:08:10 -0500

    opencv: in-progress, mostly done though

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DuckFinder.java
@@ -0,0 +1,126 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import org.apache.commons.math3.fraction.Fraction;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+
+public class DuckFinder extends OpenCvPipeline {
+    ArrayList<MatOfPoint> duckContours = new ArrayList<>();
+    Point duckCenter = null;
+    Rect duckRect;
+
+
+    // Camera Settings
+
+    protected double centerX;
+    protected double centerY;
+
+    protected int imageWidth;
+    protected int imageHeight;
+
+    private double cameraPitchOffset;
+    private double cameraYawOffset;
+
+    private double fov;
+    private double horizontalFocalLength;
+    private double verticalFocalLength;
+    private double verticalThreshold;
+
+    boolean duckOnScreen = false;
+
+    public Point getDuckCenter() {
+        if(duckOnScreen) return duckCenter;
+        else return null;
+    }
+
+    public DuckFinder(double fov, double cameraPitchOffset, double cameraYawOffset, double threshold) {
+        super();
+        this.fov = fov;
+        this.cameraPitchOffset = cameraPitchOffset;
+        this.cameraYawOffset = cameraYawOffset;
+        this.verticalThreshold = threshold;
+    }
+
+    public DuckFinder(double fov) {
+        this(fov, 0, 0, 100);
+    }
+
+    @Override
+    public void init(Mat mat) {
+        super.init(mat);
+
+        imageWidth = mat.width();
+        imageHeight = mat.height();
+
+        // pinhole model calculations
+        double diagonalView = Math.toRadians(this.fov);
+        Fraction aspectFraction = new Fraction(this.imageWidth, this.imageHeight);
+        int horizontalRatio = aspectFraction.getNumerator();
+        int verticalRatio = aspectFraction.getDenominator();
+        double diagonalAspect = Math.hypot(horizontalRatio, verticalRatio);
+        double horizontalView = Math.atan(Math.tan(diagonalView / 2) * (horizontalRatio / diagonalAspect)) * 2;
+        double verticalView = Math.atan(Math.tan(diagonalView / 2) * (verticalRatio / diagonalAspect)) * 2;
+        horizontalFocalLength = this.imageWidth / (2 * Math.tan(horizontalView / 2));
+        verticalFocalLength = this.imageHeight / (2 * Math.tan(verticalView / 2));
+    }
+
+    // A pipeline that finds and draws contours around the the ducks in the frame
+    // and makes sure all of them are below a certain y value
+    @Override
+    public Mat processFrame(Mat input) {
+        duckRect = null;
+        Mat mat = new Mat();
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+        Scalar lower = new Scalar(22, 60, 50);
+        Scalar upper = new Scalar(33, 255, 255);
+        Core.inRange(mat, lower, upper, mat);
+        Imgproc.GaussianBlur(mat, mat, new org.opencv.core.Size(9, 9), 0);
+
+        duckContours.clear();
+        Imgproc.findContours(mat, duckContours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        for (int i = 0; i < duckContours.size(); i++) {
+            Rect rect = Imgproc.boundingRect(duckContours.get(i));
+            if (rect.width > 10 && rect.height > 10 && rect.y < verticalThreshold) {
+                if (duckRect == null) {
+                    duckRect = rect;
+                } else if (rect.area() > duckRect.area()) {
+                    duckRect = rect;
+                }
+            }
+        }
+
+        Imgproc.line(mat, new Point(0, verticalThreshold), new Point(mat.width(), verticalThreshold), new Scalar(255, 0, 0), 2);
+
+        if (duckRect != null){
+            Imgproc.rectangle(input, duckRect, new Scalar(0, 255, 0));
+            duckCenter = getCenterofRect(duckRect);
+            duckOnScreen = true;
+        } else {
+            duckOnScreen = false;
+        }
+
+        mat.release();
+        return input;
+    }
+
+    public Double calculateYaw(double offsetCenterX) {
+        Double duckCenterX = null;
+        if (duckCenter != null) {
+            duckCenterX  = duckCenter.x;
+        }
+        if (duckOnScreen && duckCenterX != null) {
+            return Math.atan((duckCenterX - offsetCenterX) / horizontalFocalLength);
+        } else {
+            return null;
+        }
+    }
+
+    public Point getCenterofRect(@NonNull Rect rect) {
+        return new Point(rect.x + rect.width / 2.0, rect.y + rect.height / 2.0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Webcam.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.DuckFinder;
+import org.firstinspires.ftc.teamcode.ShippingElementRecognizer;
+import org.firstinspires.ftc.teamcode.DuckFinder;
+import org.firstinspires.ftc.teamcode.ShippingElementRecognizer;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.List;
+
+public class Webcam {
+    OpenCvWebcam webcam;
+    OpenCvWebcam frontWebcam;
+    ShippingElementRecognizer shippingElementRecognizer;
+    DuckFinder duckFinder;
+
+    public int getShippingHubLevel() {
+        return shippingElementRecognizer.getShippingHubLevel();
+    }
+
+
+    public void init(HardwareMap hardwareMap) {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        int[] viewportContainerIds = OpenCvCameraFactory.getInstance()
+                .splitLayoutForMultipleViewports(
+                        cameraMonitorViewId, //The container we're splitting
+                        2, //The number of sub-containers to create
+                        OpenCvCameraFactory.ViewportSplitMethod.HORIZONTALLY); //Whether to split the container vertically or horizontally
+        // Setup first camera
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"), viewportContainerIds[0]);
+        shippingElementRecognizer = new ShippingElementRecognizer();
+        webcam.setPipeline(shippingElementRecognizer);
+        webcam.setMillisecondsPermissionTimeout(2500);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                // This will be called if the camera could not be opened
+            }
+        });
+
+        // Second camera
+        frontWebcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Front Webcam"), viewportContainerIds[1]);
+        duckFinder = new DuckFinder(78);
+        frontWebcam.setPipeline(duckFinder);
+        frontWebcam.setMillisecondsPermissionTimeout(2500);
+        frontWebcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                frontWebcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                // This will be called if the camera could not be opened
+            }
+        });
+    }
+}
